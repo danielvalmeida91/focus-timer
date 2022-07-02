0953d3a6f4b779ff103ccfc1722faf54f752acc3
danielvalmeida91 <danielvalmeida91@gmail.com>
2022-07-02 07:10:46 -0300

    Changing <img> to <svg> and your styles.

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonPlus = document.querySelector('.plus')
+const buttonMinus = document.querySelector('.minus')
+
+const cardForest = document.querySelector('.forest')
+let minutesDisplay = document.querySelector('.minutes')
+let secondsDisplay = document.querySelector('.seconds')
+
